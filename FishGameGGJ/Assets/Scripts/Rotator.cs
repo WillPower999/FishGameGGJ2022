069456bedd102b86b3db6948a8d02e6ce8c991e7
WEBSTERAD\wustudent <dts@webster.edu>
2022-01-28 20:23:01 -0600

    More menu UI

---
+++ FishGameGGJ/Assets/Scripts/Rotator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Rotator : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public Vector3 direction;
+    public bool mouseOver = false;
+    public float speed;
+    public float initialSpeed;
+    public int nullSpeed;
+
+    void Update()
+    {
+        transform.Rotate(direction * speed);
+    }
+
+    public void OnPointerEnter(PointerEventData pointerEventData)
+    {
+        speed = nullSpeed;
+    }
+
+    public void OnPointerExit(PointerEventData pointerEventData)
+    {
+        speed = initialSpeed;
+    }
+}
