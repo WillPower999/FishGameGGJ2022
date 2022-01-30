d9beea4b17b286a4a500b62f1007ff857d1426a9
WEBSTERAD\wustudent <dts@webster.edu>
2022-01-30 17:57:33 -0600

    fix

--- FishGameGGJ/Assets/Scripts/SceneManagerClass.cs
+++ FishGameGGJ/Assets/Scripts/SceneManagerClass.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEditor.SceneManagement;
 using UnityEngine.UI;
 
 public class SceneManagerClass : MonoBehaviour
