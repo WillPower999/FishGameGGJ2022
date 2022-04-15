293d1888a23dcf3fdb779c9d5824bf19d276baff
WEBSTERAD\wustudent <dts@webster.edu>
2022-04-15 15:43:45 -0500

    buttons

---
+++ FishGameGGJ/Assets/Scripts/HoldButton.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HoldButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    [HideInInspector] public bool isPressed;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        isPressed = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        isPressed = false;
+    }
+}
---
+++ FishGameGGJ/Assets/Scripts/MovementButtons.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MovementButtons : MonoBehaviour
+{
+    [SerializeField] Button up;
+    [SerializeField] Button down;
+    [SerializeField] Button left;
+    [SerializeField] Button right;
+
+    [SerializeField] Fish fish1;
+    [SerializeField] Fish fish2;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
