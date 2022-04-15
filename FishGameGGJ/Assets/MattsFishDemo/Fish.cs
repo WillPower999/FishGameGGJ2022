c809a97d1ec566f9111f950f8461b741bae081fa
JustinBaldridge <justinbaldridge@gmail.com>
2022-04-15 14:44:25 -0500

    inital mobile porting
    switched orientation to portrait mode, movement now based on touch, minor changes to title screen.

--- FishGameGGJ/Assets/MattsFishDemo/Fish.cs
+++ FishGameGGJ/Assets/MattsFishDemo/Fish.cs
@@ -12,6 +12,9 @@ public class Fish : MonoBehaviour
     public float idleSpeed;
     public float moveSpeed;
     private bool fishStill;
+    private const float DEAD_ZONE_VALUE = 0.05f;
+    Vector2 movementInput;
+    Vector2 savedMousePosition;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -33,8 +36,81 @@ public class Fish : MonoBehaviour
         SoundManager.Instance.PlayMusic(Music.Level_Music);
     }
 
+    void Update()
+    {
+        movementInput.x = Input.GetAxisRaw("Horizontal");
+        movementInput.y = Input.GetAxisRaw("Vertical");
+
+        if (Input.touches.Length > 0)
+        {
+            Touch t = Input.touches[0];
+            if (t.phase == TouchPhase.Began)
+            {
+                savedMousePosition = t.position;
+            }
+            Vector2 input = t.position - savedMousePosition;
+            Vector2 inputNormalized = input.normalized; 
+            if (input.magnitude > DEAD_ZONE_VALUE)
+            {
+                movementInput = (Mathf.Abs(inputNormalized.x) > Mathf.Abs(inputNormalized.y))? new Vector2(Mathf.Sign(inputNormalized.x), 0): new Vector2(0, Mathf.Sign(inputNormalized.y));
+            }
+        }
+        /*
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            movementInput.x = -1;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            movementInput.x = 1;
+        }
+        else
+        {
+
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            movementInput.y = 1;
+        }   
+
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            movementInput.y = -1;
+        }*/
+    }
+
     void FixedUpdate()
     {
+        float rotation = transform.eulerAngles.y;
+        float prevSpeed = rb.velocity.magnitude;
+        if (movementInput.x != 0)
+        {
+            rb.AddForce(new Vector3(movementInput.x, 0, 0) * movementForce);
+            rotation = (movementInput.x > 0)? 90f: 270f;
+        }
+        else if (movementInput.y != 0)
+        {
+            rb.AddForce(new Vector3(0, 0, movementInput.y) * movementForce);
+            rotation = (movementInput.y > 0)? 0f: 180f;
+        }
+        transform.eulerAngles = new Vector3(0, rotation, 0);
+
+        if (rb.velocity.magnitude > 0)
+        {
+            if (prevSpeed == 0)
+            {
+                SoundManager.Instance.PlaySound(Sound.Deep_Splash);
+                fishStill = false;
+            }
+            animationClip.speed = moveSpeed;
+        }
+        else
+        {
+            animationClip.speed = idleSpeed;
+            fishStill = true;
+        }
+        /*
         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
             if (rb.velocity == Vector3.zero && fishStill)
@@ -86,6 +162,6 @@ public class Fish : MonoBehaviour
             {
                 fishStill = true;
             }
-        }
+        }*/
     }
 }
