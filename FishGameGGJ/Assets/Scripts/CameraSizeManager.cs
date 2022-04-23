4a285eb0423dbcc8face853368e82384080a02a2
WEBSTER UNIVERSITY <>
2022-04-22 19:49:27 -0500

    all teh things

--- FishGameGGJ/Assets/Scripts/CameraSizeManager.cs
+++ FishGameGGJ/Assets/Scripts/CameraSizeManager.cs
@@ -9,12 +9,6 @@ public class CameraSizeManager : MonoBehaviour
     [SerializeField] float LandscapeCameraSize;
     [SerializeField] float PortraitCameraSize;
 
-    [SerializeField] TextMeshProUGUI logText;
-
-    [SerializeField] DeviceOrientation prevOrientation;
-
-    public Transform pool;
-
     void Update()
     {
         if (Input.deviceOrientation == DeviceOrientation.Portrait || Input.deviceOrientation == DeviceOrientation.PortraitUpsideDown)
@@ -25,9 +19,5 @@ public class CameraSizeManager : MonoBehaviour
         {
             Camera.main.orthographicSize = LandscapeCameraSize;
         }
-
-        logText.SetText(Input.deviceOrientation.ToString());
-
-        pool.DORotate(Vector3.zero, 1).SetEase(Ease.InOutBack);
     }
 }
